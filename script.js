a144638ba4a90324652d122a207a93fb9c3c8caf
Wisdom2301 <freeborn.wisdom@gmail.com>
2021-06-25 11:46:07 +0100

    Week4 Update

--- script.js
+++ script.js
@@ -2,6 +2,63 @@ function myFunction(){
     window.alert("Success");
     document.getElementById("demo").innerHTML="Successfully Submitted!"; 
 }
-document.write("Wisdom");
 
 console.log(900);
+
+var name = "Wisdom";
+var age = "17";
+var rightWrong = "True";
+var numchildren = "null";
+var definition = "undefined";
+
+console.log(name);
+console.log(age);
+console.log(rightWrong);
+console.log(numchildren);
+console.log(definition);
+
+document.getElementById('name').innerHTML="Wisdom";
+document.getElementById('age').innerHTML="17";
+document.getElementById('rightWrong').innerHTML="True";
+document.getElementById('numChildren').innerHTML="null";
+document.getElementById('definition').innerHTML="undefined";
+
+var number1 = 15;
+var number2 = 12;
+var sum = number1 + number2;
+
+console.log(sum);
+document.writeln(sum);
+
+var number1 = 15;
+var number2 = 12;
+var mul = number1 * number2;
+
+console.log(mul);
+document.writeln(mul);
+
+var number1 = 15;
+var number2 = 12;
+var division = number1 / number2;
+
+console.log(division);
+document.writeln(division);
+
+var number1 = 15;
+var number2 = 12;
+var sub = number1 - number2;
+
+console.log(sub);
+document.writeln(sub);
+
+var number1 = 15;
+number1++;
+
+console.log('increment', number1);
+document.writeln(number1)
+
+var number1 = 15;
+number1--;
+
+console.log('decrement', number1);
+document.writeln(number1)
